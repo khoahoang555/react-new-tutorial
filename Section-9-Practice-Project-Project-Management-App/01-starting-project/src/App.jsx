2915d2b41bd3cee3bd392306ca703d7a596955f3
khoahoang <khoahoangyahoo@gmail.com>
2024-01-24 07:18:23 +0700

    157. Making Projects Selectable & Viewing Project Details

--- Section-9-Practice-Project-Project-Management-App/01-starting-project/src/App.jsx
+++ Section-9-Practice-Project-Project-Management-App/01-starting-project/src/App.jsx
@@ -2,12 +2,22 @@ import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import {useState} from "react";
 import NewProject from "./components/NewProject.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    })
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -18,7 +28,9 @@ function App() {
     });
   }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />;
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
   } else if (projectsState.selectedProjectId === undefined) {
@@ -52,7 +64,11 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
