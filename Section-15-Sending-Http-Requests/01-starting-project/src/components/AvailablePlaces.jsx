68329f80f10840e3c7afca32e6058f61ab0c7389
khoa pham <khoa.pham@tpssoft.com>
2024-02-05 13:15:41 +0700

    231. Sending HTTP Requests (GET Request) via useEffect

--- Section-15-Sending-Http-Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ Section-15-Sending-Http-Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,19 +1,21 @@
 import Places from './Places.jsx';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  fetch('http://localhost:3000/places').then((response) => {
-    return response.json();
-  }).then((respData) => {
-    setAvailablePlaces(respData.places);
+  useEffect(() => {
+    fetch('http://localhost:3000/places').then((response) => {
+      return response.json();
+    }).then((respData) => {
+      setAvailablePlaces(respData.places);
+    });
   })
 
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
