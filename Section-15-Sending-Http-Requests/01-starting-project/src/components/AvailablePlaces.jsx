42239b9a53ac56332eb636075947f31928974a44
khoa pham <khoa.pham@tpssoft.com>
2024-02-05 12:43:08 +0700

    230. How NOT To Send HTTP Requests (And Why It's Wrong)

--- Section-15-Sending-Http-Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ Section-15-Sending-Http-Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -4,6 +4,12 @@ import {useState} from "react";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  fetch('http://localhost:3000/places').then((response) => {
+    return response.json();
+  }).then((respData) => {
+    setAvailablePlaces(respData.places);
+  })
+
   return (
     <Places
       title="Available Places"
