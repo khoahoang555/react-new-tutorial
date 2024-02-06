a7f63e45ffff09e3116c2360f28f3c6ba9a1f4c6
khoa pham <khoa.pham@tpssoft.com>
2024-02-06 11:30:54 +0700

    238. Using Optimistic Updating (2)

--- Section-15-Sending-Http-Requests/01-starting-project/src/App.jsx
+++ Section-15-Sending-Http-Requests/01-starting-project/src/App.jsx
@@ -6,6 +6,7 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import {updateUserPlaces} from "./http.js";
+import Error from "./components/Error";
 
 function App() {
   const selectedPlace = useRef();
@@ -53,8 +54,18 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
+
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && (
+          <Error title="An error occurred!" message={errorUpdatingPlaces.message} onConfirm={handleError}/>
+        )}
+      </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
